2cb649d4edf98056d9554bb3cc0d7ad145c22d8c
Glaubert01 <glaubert@outlook.com>
2025-07-23 22:57:22 -0300

    Implementadas as logicas de conta corrente e poupanca

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -3,6 +3,8 @@ import 'package:anybank/conta.dart';
 void main() {
   Conta contaMatheus = Conta("Matheus", 1000);
   Conta contaRoberta = Conta("Roberta", 2000);
+  ContaCorrente contaChris = ContaCorrente("Chris", 4000);
+  ContaPoupanca contaDenise = ContaPoupanca("Denise", 4000);
 
   List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
 
@@ -13,4 +15,11 @@ void main() {
   contaRoberta.receberDinheiro(3000);
   contaMatheus.receberDinheiro(500);
   contaMatheus.enviarDinheiro(200);
+
+  contaChris.imprimeSaldo();
+  contaChris.enviarDinheiro(4300);
+  contaDenise.imprimeSaldo();
+  contaDenise.enviarDinheiro(4300);
+  contaDenise.calculaRendimento();
+  contaDenise.imprimeSaldo();
 }
--- lib/conta.dart
+++ lib/conta.dart
@@ -22,3 +22,29 @@ class Conta {
 
   Conta(this.titular, this._saldo);
 }
+
+class ContaCorrente extends Conta {
+  ContaCorrente(super.titular, super._saldo);
+
+  double emprestimo = 300;
+
+  @override
+  void enviarDinheiro(double valor) {
+    if (_saldo + emprestimo >= valor) {
+      _saldo -= valor;
+      imprimeSaldo();
+    } else {
+      print("Saldo mais emprestimo insuficientes para transferencia.");
+    }
+  }
+}
+
+class ContaPoupanca extends Conta {
+  double rendimento = 0.05;
+
+  void calculaRendimento() {
+    _saldo += _saldo * rendimento;
+  }
+
+  ContaPoupanca(super.titular, super._saldo);
+}
